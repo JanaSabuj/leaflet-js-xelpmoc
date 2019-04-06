afd186bd9aaa630286ca69c84569081b7cb72bb0
Sabuj Jana <sabujlovescoding@gmail.com>
2019-04-06 22:59:42 +0530

    added markers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-	 alert('This is a map');
-	 var mymap = L.map('mapid').setView([22.5726, 88.3639], 13);
+	 // alert('This is a map');
+	 var mymap = L.map('mapid').setView([22.5726, 88.3639], 12);
 
+	 //adding the basic map tile
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
@@ -9,4 +10,11 @@
 		id: 'mapbox.streets'
 	}).addTo(mymap);
 
-	
+
+
+	//markers
+
+	//add a marker for my college Jadavpur University SL Campus, JU Main Campus, 
+	var marker = L.marker([22.5605, 88.4135]).addTo(mymap).bindPopup('This is JU SL Campus').openPopup();
+	var marker = L.marker([22.4978, 88.3717]).addTo(mymap).bindPopup('This is JU Main Campus').openPopup();
+	var marker = L.marker([22.5770, 88.4313]).addTo(mymap).bindPopup('This is Millenium IT Park Xelpmoc Office').openPopup();
