b6d65554f52a4538c762a736a9687800b186fce3
Sabuj Jana <sabujlovescoding@gmail.com>
2019-04-11 18:09:55 +0530

    Kentucky map completed

--- LeafMap3/app.js
+++ LeafMap3/app.js
@@ -8,12 +8,29 @@
          alert(xhr.statusText)
      }
  })
+ var motorways = $.ajax({
+  url: "https://gist.githubusercontent.com/maptastik/df8e483d5ac1c6cae3dc4a7c02ea9039/raw/ff9897d53ee19a92e4393d23c87aa6d305e6b247/kyMotorwaysSimplified.geojson",
+  dataType: "json",
+  success: console.log("County data successfully loaded."),
+  error: function(xhr) {
+    alert(`Motorways: ${xhr.statusText}`);
+  }
+});
+
+var parks = $.ajax({
+  url: "https://gist.githubusercontent.com/maptastik/df8e483d5ac1c6cae3dc4a7c02ea9039/raw/ff9897d53ee19a92e4393d23c87aa6d305e6b247/kyParks.geojson",
+  dataType: "json",
+  success: console.log("County data successfully loaded."),
+  error: function(xhr) {
+    alert(`Parks: ${xhr.statusText}`);
+  }
+});
 
 
  //wrapping rest of the script in .when() and .done() so that AJAX call for geojson data is completed
  // first before the rest of the leaflet map loads
 
- $.when(counties).done(function() {
+ $.when(counties,motorways,parks).done(function() {
 
      var map = L.map('map').setView([37.857507, -85.632935], 7);
 
@@ -28,10 +45,38 @@
          id: 'mapbox.streets'
      }).addTo(map);
 
-      L.geoJSON(counties.responseJSON).addTo(map);
+      var marker = L.marker([38.6270,-90.1994]).addTo(map).bindPopup(' <ul> <li> Blue- All the Kentucky counties </li> <li> Red- All the Kentucky Motorways </li> <li> Black- All the Kentucky Parks </li> </ul> Im just a random notifier. <br> Zoom in for better visualization!').openPopup();
+
+      L.geoJSON(counties.responseJSON).addTo(map);	
+
+      var kyCounties = L.geoJSON(counties.responseJSON, {
+    fillOpacity: 0,
+    color: '#b2b2b2',
+    weight: 0.75
+  }).addTo(map);
+
+  var kyMotorways = L.geoJSON(motorways.responseJSON, {
+    color: 'red',
+    weight: 1
+  }).addTo(map);
+  
+  var kyParks = L.geoJSON(parks.responseJSON, {
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng, {
+        radius: 4,
+        fillOpacity: 0,
+        color: 'black',
+        weight: 0.75
+      })
+    }
+  }).addTo(map);
 
 
- });
+});
+
+
+
+ 
 
  //displaying featurers of the data
 
