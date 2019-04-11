fc6c2f11fc3a6d4e351d6ccf900d3f8bd238a1f1
Sabuj Jana <sabujlovescoding@gmail.com>
2019-04-11 18:47:08 +0530

    new local ajax

---
+++ LeafMap4/app.js
@@ -0,0 +1,28 @@
+ 
+     var map = L.map('map').setView([37.857507, -85.632935], 7);
+
+
+
+     //adding the basic map tile
+     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+         maxZoom: 18,
+         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+             '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+             'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+         id: 'mapbox.streets'
+     }).addTo(map);
+     
+     function basement(feature, layer){
+
+      layer.bindPopup(" <h1>Hi Im info </h1>");
+
+     };
+
+
+    L.geoJSON(kyCounties,{
+      onEachFeature: basement
+
+    }).addTo(map);
+ 
+ 
+ 
